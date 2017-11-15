ee88a611c0192d5c78e3219b9dc78d96ac221bd1
Idar Syslak <idasys1@outlook.com>
2017-11-15 18:51:10 +0100

    Remove redundant css imports

--- react-auction/src/components/Navbar.js
+++ react-auction/src/components/Navbar.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import './../styles/App.css';
 import Login from './Login.js';
 import Loggedin from './Loggedin.js';
 import './../styles/navbar.css';
 
 class Navbar extends Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     let loginModule = null;
